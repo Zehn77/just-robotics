c1592212bb513ea0b9521803292ff3ee2ac2853e
Zehn77 <zekhn777@gmail.com>
2025-05-14 14:19:19 +0500

    fixed small bug related to the vercel

--- src/components/courses/data.ts
+++ src/components/courses/data.ts
@@ -1,31 +1,31 @@
-export const courses = [
+export const data = [
   {
     id: 0,
-    url: "assets/images/children-with-robots.jpg",
+    url: "images/children-with-robots.jpg",
     title: "Робототехника",
     alt: "Children with robots",
   },
   {
     id: 1,
-    url: "assets/images/children-artist.jpg",
+    url: "images/children-artist.jpg",
     title: "Живопись для детей",
     alt: "Children artist",
   },
   {
     id: 2,
-    url: "assets/images/adults-artist.jpg",
+    url: "images/adults-artist.jpg",
     title: "Живопись для взрослых",
     alt: "Adults artist.jpg",
   },
   {
     id: 3,
-    url: "assets/images/children-reading.jpg",
+    url: "images/children-reading.jpg",
     title: "Английский язык для детей",
     alt: "Children reading",
   },
   {
     id: 4,
-    url: "assets/images/children-playing-chess.png",
+    url: "images/children-playing-chess.png",
     title: "Шахмат",
     alt: "Children playing chess",
   },
