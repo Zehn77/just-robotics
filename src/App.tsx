c3ebf91f822325bb26f0847fae1acb2f77bb6262
Zehn77 <zekhn777@gmail.com>
2025-05-14 14:32:14 +0500

    feat: Footer is under constructing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Cards } from "./components/cards/Cards";
 import { Courses } from "./components/courses/Courses";
 import { Description } from "./components/desctiption/Description";
+import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { Hero } from "./components/hero/Hero";
 import { LessonsForm } from "./components/lessons-form/LessonsForm";
@@ -25,6 +26,8 @@ export default function App() {
       <Reviews />
 
       <LessonsForm />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="h-52 flex justify-center items-center">
+      <p className="text-white text-2xl">Here are will be footer</p>
+    </footer>
+  );
+};
