6f749f59767bcce7b62fe8c932ec8ae90ec3389a
Zehn77 <zekhn777@gmail.com>
2025-05-14 14:13:53 +0500

    fix: bug with versel fonts url

--- src/components/courses/data.ts
+++ src/components/courses/data.ts
@@ -1,31 +1,31 @@
 export const courses = [
   {
     id: 0,
-    url: "images/children-with-robots.jpg",
+    url: "assets/images/children-with-robots.jpg",
     title: "Робототехника",
     alt: "Children with robots",
   },
   {
     id: 1,
-    url: "images/children-artist.jpg",
+    url: "assets/images/children-artist.jpg",
     title: "Живопись для детей",
     alt: "Children artist",
   },
   {
     id: 2,
-    url: "images/adults-artist.jpg",
+    url: "assets/images/adults-artist.jpg",
     title: "Живопись для взрослых",
     alt: "Adults artist.jpg",
   },
   {
     id: 3,
-    url: "images/children-reading.jpg",
+    url: "assets/images/children-reading.jpg",
     title: "Английский язык для детей",
     alt: "Children reading",
   },
   {
     id: 4,
-    url: "images/children-playing-chess.png",
+    url: "assets/images/children-playing-chess.png",
     title: "Шахмат",
     alt: "Children playing chess",
   },
